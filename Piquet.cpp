1a9c477e32860efcd1552210745682483317d77c
Admin <sergeyyysap@gmail.com>
2023-04-18 18:46:27 +0300

    Realize firstasssasdadsass=saassadaasadssaass\sdza=sadsfsa=dsaadasdssdasdsas

---
+++ Piquet.cpp
@@ -0,0 +1,35 @@
+#include "Piquet.h"
+
+Piquet::Piquet(int lenght, double shift): lenght(lenght), shift(shift)
+{}
+
+bool Piquet::IsStandart()const
+{
+    return lenght == 100;
+}
+
+std::string ToString(const Piquet &piquet)
+{
+    return (piquet.lenght < 0 ? "(ПК 0": "(ПК ") + std::to_string(piquet.lenght) + "+" + std::to_string(piquet.shift) + ")";   
+}
+
+std::istream& operator>>(std::istream &istream, Piquet &piquet)
+{
+    istream >> piquet.lenght;
+    istream >> piquet.shift;
+    return istream;
+}
+std::ostream& operator<<(std::ostream &ostream, Piquet &piquet)
+{
+    return ostream << ToString(piquet);
+}
+
+bool operator==(const Piquet &lha, const Piquet &rha)
+{
+    return lha.lenght == lha.lenght && rha.shift == rha.shift;
+}
+
+bool operator!=(const Piquet &lha, const Piquet &rha)
+{
+    return not operator==(lha, rha);
+}
---
+++ Piquet.h
@@ -0,0 +1,37 @@
+#include <string>
+
+
+class Piquet
+{
+private:
+    int lenght;
+    double shift;
+public:
+    
+    /**
+     * @brief Считает пикет и смещение 
+     * 
+     */
+    Piquet(int lenght, double shift);
+
+    /**
+     * @brief Проверяет равна ли длинна 100 метрам
+     * 
+     * @param lenght длинна пикета
+     * @return true когда lenght = 100 метров
+     * @return false когда lenght != 100 метров
+     */
+    bool IsStandart()const;
+    
+    /**
+     * @brief Переводит пикет и смещение в строку
+     * 
+     * @return std::string пикет и смещение в виде строки
+     */
+    friend std::string ToString(const Piquet& piquet);
+
+    friend std::istream& operator>>(std::istream& istream, Piquet& piquet);
+    friend std::ostream& operator<<(std::ostream& ostream, Piquet& piquet);
+    friend bool operator==(const Piquet& lha, const Piquet& rha);
+    friend bool operator!=(const Piquet& lha, const Piquet& rha);
+};
