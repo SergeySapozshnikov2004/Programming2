f5c7c7cb9a0114c9950adecf7a982bf7300fdd77
Admin <sergeyyysap@gmail.com>
2023-05-16 19:14:43 +0300

    Добавил по условию

--- Curve.cpp
+++ Curve.cpp
@@ -3,3 +3,13 @@
 
 Curve::Curve(double radius, double start, double end): radius(radius), start(start), end(end)
 {}
+
+std::string ToString(const Curve &curve)
+{
+    return (curve.radius < 0 ? "(ПК 0": "(ПК ") + std::to_string(curve.start) + "+" + std::to_string(curve.end) + ")";   
+}
+
+std::ostream& operator<<(std::ostream &ostream, Curve &curve)
+{
+    return ostream << ToString(curve);
+}
--- Curve.h
+++ Curve.h
@@ -13,4 +13,16 @@ public:
     //  * 
      */
     Curve(const double radius, const int start,const int end);
+
+        /**
+     * @brief Переводит пикет и смещение в строку
+     * 
+     * @return std::string пикет и смещение в виде строки
+     */
+    friend std::string ToString(const Curve& curve);
+    friend std::string ToString(const Curve* curve);
+    friend std::string ToString(Curve* curve);
+
+
+    friend std::ostream& operator<<(std::ostream& ostream, Curve& curve);
 };
